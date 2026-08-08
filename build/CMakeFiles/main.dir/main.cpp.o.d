c28e256f2c0aa0fed17e0d797f181f036d1855e3
FelipeOG1 <feliog1234@gmail.com>
2026-08-08 15:15:42 -0600

    build setup

---
+++ build/CMakeFiles/main.dir/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/main.dir/main.cpp.o: /home/felipe/irc_project/main.cpp \
+ /usr/include/stdc-predef.h
--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 
 int main() {
+	int values[3]{2, 3};
+  int v2[3];
+  for (int i=0; i < 3;++i) v2[i] = values[i];
+  
   return 0;
 }
